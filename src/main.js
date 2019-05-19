a27b3823fd6417350100d94013e1a450c23482ac
niri4 <nirankarsingh247@gmail.com>
2019-05-19 21:57:56 +0530

    add font-awesome

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas)
 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
